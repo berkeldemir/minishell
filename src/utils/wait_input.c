c821ecfed3a8b55a769da37e1ffbba710e8e4577
Tibet Mıdık <tmidik@k1m14s01.42istanbul.com.tr>
2025-06-20 15:46:07 +0300

    HOME dizini tilde (~) işareti ile değiştirildi. Böylelikle daha güzel bir görünüm sağlandı

--- src/utils/wait_input.c
+++ src/utils/wait_input.c
@@ -6,22 +6,62 @@
 /*   By: tmidik <tibetmdk@gmail.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 11:19:42 by beldemir          #+#    #+#             */
-/*   Updated: 2025/06/17 17:12:45 by tmidik           ###   ########.fr       */
+/*   Updated: 2025/06/20 15:43:51 by tmidik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-static void	prompt(t_data *data)
+static char	*ms_ft_strdup(char *s)
+{
+	int		i;
+	char	*toreturn;
+
+	i = -1;
+	toreturn = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1));
+	if (!toreturn)
+		return (NULL);
+	while (s[++i] != '\0')
+		toreturn[i] = s[i];
+	toreturn[i] = '\0';
+	return (toreturn);
+}
+
+static char	*get_display_path(char *path)
+{
+	char	*home;
+	char	*result;
+
+	home = getenv("HOME");
+	if (home && strncmp(path, home, ms_ft_strlen(home)) == 0)
+	{
+		if (path[ms_ft_strlen(home)] == '\0')
+			return (ms_ft_strdup("~"));
+		result = ms_ft_strjoin(path + ms_ft_strlen(home), "~");
+		return (result);
+	}
+	return (strdup(path));
+}
+
+static void	print_prompt(char *path)
+{
+	char	*display;
+
+	display = get_display_path(path);
+	printf("%s%s@:%s %s\n%s",
+		GREEN, getenv("USER"), ORANGE, display, DEFAULT);
+	free(display);
+}
+
+void	wait_input(t_data *data)
 {
 	char	path[1023];
 
-	//data->user = getenv("USER");
 	while (1)
 	{
 		if (getcwd(path, sizeof(path)) == NULL)
 			return ;
-		printf("%s%s@:%s %s\n%s", GREEN, getenv("USER"), ORANGE, path, DEFAULT);
+		print_prompt(path);
 		data->input = readline("\033[38;2;8;99;117m>₺ \033[0m");
 		if (!data->input)
 			break ;
@@ -32,9 +72,4 @@ static void	prompt(t_data *data)
 		}
 		free(data->input);
 	}
-}
-
-void	wait_input(t_data *data)
-{
-	prompt(data);
 }
